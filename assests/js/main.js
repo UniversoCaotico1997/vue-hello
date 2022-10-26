46fc6a70e5ab74a73c7dec82bce024297e427e9a
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-26 15:31:29 +0200

    start Vue Hello

---
+++ assests/js/main.js
@@ -0,0 +1,14 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
